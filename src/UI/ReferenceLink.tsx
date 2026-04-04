4b0a25ec0730752db93ed35c391f6b7f0ac462ba
mjclaypool <148149089+mjclaypool@users.noreply.github.com>
2026-04-04 09:42:27 -0400

    Add reference links to chat

---
+++ src/UI/ReferenceLink.tsx
@@ -0,0 +1,29 @@
+import { useContext } from "react"
+import { IoLink } from "react-icons/io5";
+
+import TaskContext from "../store/TaskContext"
+
+export default function ReferenceLink() {
+    const taskCtx = useContext(TaskContext)
+
+    return (
+        <div className="flex items-center self-start bg-cc-sec max-w-[230px] min-h-[40px] rounded-lg p-2">
+            <p className="text-cc-offw text-[12px] whitespace-pre-wrap">AI Reference Links: </p>
+            {!taskCtx?.task.references.includes("No references available") ? taskCtx?.task.references.map((reference, index) => (
+                <a
+                    key={index}
+                    href={reference}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center ml-1">
+                    <button type="button">
+                        <IoLink className="text-cc-offw h-[20px] w-[20px]" />
+                    </button>
+                </a>
+            ))
+            :
+                <p className="text-cc-offw text-[12px] ml-1">Not available</p>
+            }
+        </div>
+    )
+}
--- src/components/ChatDialog.tsx
+++ src/components/ChatDialog.tsx
@@ -4,6 +4,7 @@ import ChatInputWrapper from "./ChatInputWrapper";
 import ChatOutputWrapper from "./ChatOutputWrapper";
 
 import TaskContext from "../store/TaskContext";
+import ReferenceLink from "../UI/ReferenceLink";
 
 export default function ChatDialog() {
     const taskCtx = useContext(TaskContext);
@@ -15,9 +16,11 @@ export default function ChatDialog() {
 
     return (
         <div className="w-full flex flex-col gap-[18px] overflow-y-auto">
+            {/* Can index in task list be used to render the corrent referenceLinks? */}
             {taskCtx?.chatDialog.map((msg, index) => (
-                <div key={index} className="flex flex-col">
+                <div key={index} className="flex flex-col gap-4">
                     {index % 2 == 0 ? <ChatOutputWrapper chatOutput={msg} /> : <ChatInputWrapper userInput={msg} />}
+                    {msg.includes("Typically this activity occurs every") && <ReferenceLink />}
                 </div>
             ))}
             {taskCtx?.isThinking && <ChatOutputWrapper chatOutput="..." />}
